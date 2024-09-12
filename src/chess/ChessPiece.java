4f0032318a7b30f4afb333039d886e2b74bbd1c7
Jhonny Silva.house <jhonnyms1012@gmail.com>
2024-09-11 23:45:49 -0300

    Possible moves of a piece

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,8 +2,10 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
-public class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece{
+	
 	private Color color;
 
 	public ChessPiece(Board board, Color color) {
@@ -14,6 +16,5 @@ public class ChessPiece extends Piece{
 	public Color getColor() {
 		return color;
 	}
-
 	
 }
